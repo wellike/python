ab32ddbe0043bd0d1c39eb55b827e3429717d761
wellike <1647882779@qq.com>
2017-06-04 15:45:39 +0800

    Initial commit

---
+++ 知乎登录.py
@@ -0,0 +1,74 @@
+# encoding: utf-8
+# !/usr/bin/env python
+"""
+
+"""
+import time
+from http import  cookiejar
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    "Host": "www.zhihu.com",
+    "Referer": "https://www.zhihu.com/",
+    'User-Agent': 'User-Agent:Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Mobile Safari/537.36'
+}
+# 使用登录cookie信息
+session = requests.session()
+session.cookies = cookiejar.LWPCookieJar(filename='cookies.txt')
+try:
+
+    print(session.cookies)
+    session.cookies.load(ignore_discard=True)
+
+except:
+    print("还没有cookie信息")
+
+
+def get_xsrf():
+    response = session.get("https://www.zhihu.com", headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    xsrf = soup.find('input', attrs={"name": "_xsrf"}).get("value")
+    return xsrf
+
+
+def get_captcha():
+    """
+    把验证码图片保存到当前目录，手动识别验证码
+    :return:
+    """
+    t = str(int(time.time() * 1000))
+    captcha_url = 'https://www.zhihu.com/captcha.gif?r=' + t + "&type=login"
+    r = session.get(captcha_url, headers=headers)
+    with open('captcha.jpg', 'wb') as f:
+        f.write(r.content)
+    captcha = input("验证码：")
+    return captcha
+def login(email, password):
+    login_url = 'https://www.zhihu.com/login/email'
+    data = {
+        'email': email,
+        'password': password,
+        '_xsrf': get_xsrf(),
+        "captcha": get_captcha(),
+        'remember_me': 'true'}
+    response = session.post(login_url, data=data, headers=headers)
+    login_code = response.json()
+    print(login_code['msg'])
+    for i in session.cookies:
+        print(i)
+    session.cookies.save()
+
+
+if __name__ == '__main__':
+    email = "xxxx"
+    password = "xxxxx"
+    login(email, password)
+
+
+
+
+
+
+
+
